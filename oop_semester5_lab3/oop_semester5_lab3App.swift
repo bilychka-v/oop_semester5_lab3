411b285458c85f523e6c1222de4c50a5d8ac2f9b
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2024-11-10 02:44:24 +0200

    Initial Commit

---
+++ oop_semester5_lab3/oop_semester5_lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  oop_semester5_lab3App.swift
+//  oop_semester5_lab3
+//
+//  Created by Вікторія Білик on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct oop_semester5_lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
