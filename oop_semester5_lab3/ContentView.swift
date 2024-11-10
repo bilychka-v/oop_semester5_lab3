3e5a15f045d1d37c347904a0d967cd634f27d508
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2024-11-10 13:54:05 +0200

    added small design

--- oop_semester5_lab3/ContentView.swift
+++ oop_semester5_lab3/ContentView.swift
@@ -1,40 +1,87 @@
-//
-//  ContentView.swift
-//  oop_semester5_lab3
-//
-//  Created by Вікторія Білик on 10.11.2024.
-//
-
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.black // Встановлюємо фоновий колір
-                .ignoresSafeArea() // Заповнюємо весь екран, включаючи області за межами safe area
-            
-            VStack(alignment: .leading) {
-                Text("Calories: ")
+        ZStack{
+            Color.black.ignoresSafeArea()
+            VStack(spacing:20){
+                Text("Menu")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                    .foregroundColor(Color.red)
-                
-                HStack(alignment: .top) {
-                    Text("Steps: ")
-                        .font(.title)
-                        .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                HStack{
+                    Image(systemName: "flame.fill")
+                        .foregroundColor(.pink)
+                    Text("Calories: ")
+                        .font(.title2)
                         .foregroundColor(Color.white)
                     Spacer()
-                    Text("Distance: ")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
+                    Text("0 kcal")
+                        .font(.title2)
+                        .foregroundColor(Color.pink)
                 }
-                Spacer() // Додаємо простір унизу, щоб вміст був вгорі
+                .padding()
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(10)
+                
+                ProgressCircle(progress: 0.5,color: .pink)
+                    .frame(width: 100, height: 100) // Adjust the size as needed
+                    .padding(.top, 10)
+                
+                HStack{
+                    Image(systemName: "figure.walk")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text("Steps: ")
+                        .font(.title2)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                    Text("0")
+                        .font(.title2)
+                        .foregroundColor(Color.blue)
+                }
+                .padding()
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(10)
+                HStack{
+                    Image(systemName: "location")
+                        .foregroundColor(.green)
+                    Text("Distance: ")
+                        .font(.title2)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                    Text("0 km")
+                        .font(.title2)
+                        .foregroundColor(Color.green)
+                }
+                .padding()
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(10)
+                Spacer()
             }
-            .padding(.horizontal, 30)
-            .padding(.top, 30)
+            .padding(.top, 50)
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct ProgressCircle: View {
+    var progress: Double
+    var color: Color
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 20)
+                .foregroundColor(color.opacity(0.3))
+            
+            Circle()
+                .trim(from: 0.0, to: CGFloat(progress))
+                .stroke(color, lineWidth: 20)
+                .rotationEffect(.degrees(-45))
+            
+            Text("\(Int(progress * 100))%")
+                .font(.caption)
+                .foregroundColor(.white)
         }
     }
 }
@@ -42,5 +89,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-
