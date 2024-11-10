b31f3106d208f68cb4d7486169996ace32d695ef
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2024-11-10 02:45:30 +0200

    .

--- oop_semester5_lab3/ContentView.swift
+++ oop_semester5_lab3/ContentView.swift
@@ -5,20 +5,42 @@
 //  Created by Вікторія Білик on 10.11.2024.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black // Встановлюємо фоновий колір
+                .ignoresSafeArea() // Заповнюємо весь екран, включаючи області за межами safe area
+            
+            VStack(alignment: .leading) {
+                Text("Calories: ")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.red)
+                
+                HStack(alignment: .top) {
+                    Text("Steps: ")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                    Text("Distance: ")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                }
+                Spacer() // Додаємо простір унизу, щоб вміст був вгорі
+            }
+            .padding(.horizontal, 30)
+            .padding(.top, 30)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
